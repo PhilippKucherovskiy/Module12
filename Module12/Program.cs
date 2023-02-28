fd36c19b894c35eb150b4f3ef60aa023d78b3c1b
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-02-28 23:15:35 +0300

    Task12.SubTask1.2

--- Module12/Program.cs
+++ Module12/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Как Вас зовут");
-string name = Console.ReadLine();
-string greetings = "Привет" + name;
-Console.WriteLine(greetings);
+﻿void Greeting ()
+{
+Console.WriteLine("Как Вас зовут");
+string? name = Console.ReadLine();
+string greetings = "Привет, " + name;
+    Console.WriteLine(greetings); }
+Greeting();
